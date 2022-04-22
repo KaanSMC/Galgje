e4aabd7dcc48e2ffb2b2828273222684fbbdbdff
ChristiaanDD <93121676+ChristiaanDD@users.noreply.github.com>
2022-04-22 08:01:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+print("Welkom bij Galgje!")
+
+woorden = (" “informatica”, “informatiekunde”, “spelletje”, “aardigheidje”, “scholier”, “fotografie”, “waardebepaling”, “specialiteit”, “verzekering”, “universiteit” en “heesterperk”.")
+print(random(0,10))
+ 
+
