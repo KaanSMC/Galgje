ac41c7c54f92d7d5e7ed3d3fa81e139b63f69d66
Kaan Duman <93122573+KaanSMC@users.noreply.github.com>
2022-05-13 07:06:19 +0000

    print random choice

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import random
 print("Welkom bij Galgje!")
 
 woorden = (" “informatica”, “informatiekunde”, “spelletje”, “aardigheidje”, “scholier”, “fotografie”, “waardebepaling”, “specialiteit”, “verzekering”, “universiteit” en “heesterperk”.")
-print(random(0,10))
+print(random.choice(woorden))
  
 
